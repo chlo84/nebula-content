f56d09ae7a49c8221f7f2cccb9a6951e3a9140ff
Chloe <chloe84@msn.com>
2022-01-10 17:46:44 -0800

    higher order functions primer

---
+++ codewars-level7-review/higherorderfunctions-premier.js
@@ -0,0 +1,18 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+// true if at least one Ruby developer has signed up; or
+// false if there will be no Ruby developers.
+
+
+function isRubyComing(list) {
+    let output = false;
+  //   let output2 = true;
+    for(let i = 0; i < list.length; i++){
+      if(list[i].language === 'Ruby')
+        return true;
+    
+  }
+     return output 
+    } 
